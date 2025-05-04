a13b209c080b0c99e351aedcca0f149672c7625d
tp-martin <martin.tyler.p@gmail.com>
2025-05-03 21:20:40 -0400

    Add player prompt and rounds add player prompt be sure to include console.log to have messages after each round add game function to include best of 5 rounds

--- js.js
+++ js.js
@@ -1,9 +1,14 @@
+let playerScore = 0
+let compScore = 0
+
+
 const computerPlay = () => {
     const arrOfChoices = ['rock' , 'paper' , 'scissors']
     const randomNum = Math.floor(Math.random() * 3)
     const compChoice = arrOfChoices[randomNum]
     return compChoice
 }
+
 
 const playRound = (playerSelection, computerSelection) => {
     if (playerSelection === 'rock' && computerSelection === 'rock') {
@@ -13,16 +18,41 @@ const playRound = (playerSelection, computerSelection) => {
     } else if (playerSelection === 'paper' && computerSelection === 'paper') {
         return 'You tied! You both picked paper.'
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore++
         return 'You won! Rock smooshes scissors!'
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        compScore++
         return 'You lost! Paper covers rock.'
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore++
         return 'You won! Paper covers rock.'
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        compScore++
         return 'You lost! Scissors cut paper.'
-    }else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore++
         return 'You won! Scissors cut paper.'
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        compScore++
         return 'You lost! Rock smooshes scissors.'
     }
 }
+
+const game = () => {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt('Choose your character', 'Rock, Paper, Scissors').toLowerCase()
+        const computerSelection = computerPlay()
+        console.log('1 ', playRound(playerSelection, computerSelection))
+    }  
+
+
+    if (playerScore > compScore) {
+        return 'You beat the computer! Nice Work!'
+    } else if (playerScore < compScore) {
+        return 'The computer beat you. Shameful.'
+    } else {
+        return 'You tied somehow.'
+    }
+}
+
+console.log(game())
